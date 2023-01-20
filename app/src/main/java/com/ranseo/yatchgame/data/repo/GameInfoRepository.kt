9577809c9d49a53a73a847d87147889c55961523
ranseo <slcksseo@gmail.com>
2023-01-20 21:33:45 +0900

    Statis
    suspend fun getMyGameScoreAndPlayer(player: Player) = withContext(Dispatchers.Default) {
            Transformations.map(myGameScoreAndPlayerLocalDataSource.getMyGameScoreAndPlayer(player)) { list ->
    
                var draw : Int = 0
                var win : Int = 0
                var lose : Int = 0
                for((gameResult, first, second) in list) {
                    if(DRAW_FLAG.containsMatchIn(gameResult)) draw++
                    val playerName = gameResult.substringAfter(WIN_FLAG).substringBefore("님")
                    if(first != null) {
    
                    } else {
    
                    }
    
                }
            }
        }

--- app/src/main/java/com/ranseo/yatchgame/data/repo/GameInfoRepository.kt
+++ app/src/main/java/com/ranseo/yatchgame/data/repo/GameInfoRepository.kt
@@ -6,6 +6,8 @@ import androidx.lifecycle.Transformations
 import com.ranseo.yatchgame.LogTag
 import com.ranseo.yatchgame.R
 import com.ranseo.yatchgame.data.model.*
+import com.ranseo.yatchgame.data.model.gameinfo.MyGameScoreAndPlayer
+import com.ranseo.yatchgame.data.model.statis.BestScore
 import com.ranseo.yatchgame.data.model.statis.WinDrawLose
 import com.ranseo.yatchgame.data.source.*
 import com.ranseo.yatchgame.log
@@ -49,7 +51,7 @@ class GameInfoRepository @Inject constructor(
             var win: Int = 0
             var lose: Int = 0
             try {
-                for ((gameResult, gameScore, firstPlayer, secondPlayer) in list) {
+                for ((gameResult, gameScore, firstPlayer, secondPlayer, _) in list) {
 
                     log(
                         TAG,
@@ -87,6 +89,40 @@ class GameInfoRepository @Inject constructor(
             } catch (error: Exception) {
                 log(TAG, "getMyGameScoreAndPlayer error = ${error.message}", LogTag.I)
                 WinDrawLose(win, draw, lose)
+            }
+        }
+
+
+    fun getBestScore(player: Player) : LiveData<BestScore> =
+        Transformations.map(myGameScoreAndPlayerLocalDataSource.getMyGameScoreAndPlayer(player)) { list ->
+            var bestScore = 0
+            var boards: List<Board> = listOf()
+            var firstPlayer :String = ""
+            var secondPlayer:String = ""
+
+            try {
+                val first : MyGameScoreAndPlayer? = list.maxByOrNull { it.gameScore.substringBefore(" :").toInt()}
+                val second : MyGameScoreAndPlayer? = list.maxByOrNull { it.gameScore.substringAfter(": ").toInt()}
+
+                if(first != null && first.firstPlayer.playerId == player.playerId) {
+                    bestScore = first.gameScore.substringBefore(" :").toInt()
+                    boards = first.boards
+                    firstPlayer= first.firstPlayer.name
+                    secondPlayer = first.secondPlayer.name
+                } else if(second != null && second.secondPlayer.playerId == player.playerId) {
+                    bestScore = second.gameScore.substringAfter(": ").toInt()
+                    boards = second.boards
+                    firstPlayer = second.firstPlayer.name
+                    secondPlayer = second.secondPlayer.name
+                }
+
+                BestScore(bestScore, boards, firstPlayer, secondPlayer)
+            } catch (error:Exception) {
+                log(TAG,"getBestScore() : ${error.message}",LogTag.I)
+                BestScore(bestScore, boards, firstPlayer, secondPlayer )
+            } catch (error:NullPointerException) {
+                log(TAG,"getBestScore() : ${error.message}",LogTag.I)
+                BestScore(bestScore, boards, firstPlayer, secondPlayer )
             }
         }
 
--- app/src/main/java/com/ranseo/yatchgame/ui/lobby/statis/StatisViewModel.kt
+++ app/src/main/java/com/ranseo/yatchgame/ui/lobby/statis/StatisViewModel.kt
@@ -1,10 +1,16 @@
 package com.ranseo.yatchgame.ui.lobby.statis
 
 import android.app.Application
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.*
+import com.ranseo.yatchgame.R
+import com.ranseo.yatchgame.data.model.Board
+import com.ranseo.yatchgame.data.model.BoardRecord
 import com.ranseo.yatchgame.data.model.GameInfo
 import com.ranseo.yatchgame.data.model.Player
+import com.ranseo.yatchgame.data.model.statis.BestScore
 import com.ranseo.yatchgame.data.model.statis.WinDrawLose
+import com.ranseo.yatchgame.domain.usecase.get.GetBestScoreUseCase
 import com.ranseo.yatchgame.domain.usecase.get.GetGameInfosUseCase
 import com.ranseo.yatchgame.domain.usecase.get.GetPlayerUseCase
 import com.ranseo.yatchgame.domain.usecase.get.GetWinDrawLoseUseCase
@@ -16,6 +22,7 @@ import javax.inject.Inject
 class StatisViewModel @Inject constructor(
     getPlayerUseCase: GetPlayerUseCase,
     private val getWinDrawLoseUseCase: GetWinDrawLoseUseCase,
+    private val getBestScoreUseCase: GetBestScoreUseCase,
     application: Application
 ) : AndroidViewModel(application) {
 
@@ -24,6 +31,21 @@ class StatisViewModel @Inject constructor(
     lateinit var winDrawLose : LiveData<WinDrawLose>
     lateinit var winDrawLoseStr : LiveData<String>
     lateinit var winRate : LiveData<String>
+
+    lateinit var bestScore : LiveData<BestScore>
+    lateinit var bestScoreStr : LiveData<String>
+    lateinit var bestFirstBoard : LiveData<Board?>
+    lateinit var bestSecondBoard : LiveData<Board?>
+
+
+    val boardRecord : LiveData<BoardRecord> = liveData { BoardRecord(arrayOf(true,true,true,true,true,true,true,true,true,true,true,true)) }
+    val emptyBoard : LiveData<Board> = liveData { Board() }
+    val turnCountStr : LiveData<String> = liveData {""}
+    val turnLight : LiveData<Int> = liveData { ContextCompat.getColor(getApplication(), R.color.black)}
+    val
+    init {
+    }
+
 
     fun getWinDrawLose(player:Player) {
         winDrawLose = getWinDrawLoseUseCase(player)
@@ -35,6 +57,12 @@ class StatisViewModel @Inject constructor(
             else "승률 : ${String.format("%.2f", (winDrawLose.win).toDouble()/(winDrawLose.win + winDrawLose.lose).toDouble() * 100)}%"
         }
     }
+
+    fun getBestScore(player:Player) {
+
+    }
+
+
 
 
 
